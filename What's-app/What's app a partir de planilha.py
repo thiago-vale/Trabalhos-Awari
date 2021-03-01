a0c82d90ca3b727530c89c75a3d33f70045328c0
THIAGO VALE <77752211+thiago-vale@users.noreply.github.com>
2021-03-01 10:09:40 -0300

    Update What's app a partir de planilha.py

--- What's-app/What's app a partir de planilha.py
+++ What's-app/What's app a partir de planilha.py
@@ -3,11 +3,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 import pandas as pd
 
-NEW_CHAT = '//*[@id="side"]/header/div[2]/div/span/div[2]/div/span'
+NEW_CHAT = '//*[@id="side"]/header/div[2]/div/span/div[2]/div'
 CONTACT_FIELD = '//*[@id="app"]/div/div/div[2]/div[1]/span/div/span/div/div[1]/div/label/div/div[2]'
-PRIMEIRO_CONTATO = '//*[@id="app"]/div/div/div[2]/div[1]/span/div/span/div/div[2]/div[1]/div/div/div[2]/div/div/div[2]'
-MSG_TYPE = '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]'
-SEND_MSG = '//*[@id="main"]/footer/div[1]/div[3]'
+PRIMEIRO_CONTATO = 'TbtXF'
+MSG_BOX = '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]'
+SEND_MSG = "_1E0Oz"
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -22,14 +22,13 @@ def enviar_mensagem(contato, mensagem):
     contact_field.click()
     contact_field.send_keys(contato)
     sleep(2)
-    primeiro_contato = driver.find_element_by_xpath(PRIMEIRO_CONTATO)
+    primeiro_contato = driver.find_element_by_class_name(PRIMEIRO_CONTATO)
     primeiro_contato.click()
-    sleep(2)
-    msg_type = driver.find_element_by_xpath(MSG_TYPE)
+    sleep(3)
+    msg_type = driver.find_element_by_xpath(MSG_BOX)
     msg_type.click()
     msg_type.send_keys(mensagem)
-    sleep(2)
-    send_msg = driver.find_element_by_xpath(SEND_MSG)
+    send_msg = driver.find_element_by_class_name(SEND_MSG)
     send_msg.click()
     sleep(1)
 
